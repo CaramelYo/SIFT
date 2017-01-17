89aa7a1bf994338e2f0993503a36fbe8da088658
CaramelYo <alltime326@gmail.com>
2017-01-17 15:00:29 +0800

    Add project files.

---
+++ SIFT/SIFT.h
@@ -0,0 +1,32 @@
+
+// SIFT.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSIFTApp:
+// See SIFT.cpp for the implementation of this class
+//
+
+class CSIFTApp : public CWinApp
+{
+public:
+	CSIFTApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSIFTApp theApp;
